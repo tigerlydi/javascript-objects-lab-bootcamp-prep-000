83c6fde83bad3c48d03ca56a40515565bea652e3
Lydia K <lydik369@gmail.com>
2019-06-24 16:38:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var recipes = {}
-
+//made an empty object
 function updateObjectWithKeyAndValue(object, key, value) {
     return Object.assign({}, object, {[key]: value})
 }
+//{} creates a new object, put in an existing object, new key and values?
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
     object[key] = value
